237a4cd0e04602035cbd63ec0ce40c1c31ee7f1d
zongdeiqianxing <34082644+zongdeiqianxing@users.noreply.github.com>
2021-08-31 16:18:09 +0800

    Update unauthorized-check.py

--- script/unauthorized-check.py
+++ script/unauthorized-check.py
@@ -1,5 +1,14 @@
 '''
 https://github.com/cwkiller/unauthorized-check/blob/master/unauthorized-check.py
+
+扫描以下常见未授权访问
+redis、mongodb、memcached、elasticsearch、zookeeper、ftp、CouchDB、docker、Hadoop
+
+安装
+pip3 install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple/
+
+使用说明
+python3 unauthorized-check.py url.txt
 '''
 import socket
 import pymongo
